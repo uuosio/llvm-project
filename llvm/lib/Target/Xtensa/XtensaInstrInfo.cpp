c0376b3d445b7b44c17cd454d6b64d5fa0247d08
Andrei Safronov <safronov@espressif.com>
2021-07-23 15:44:15 +0300

    [Xtensa] Fix Hardware Loop optimization
    Closes #43

--- llvm/lib/Target/Xtensa/XtensaInstrInfo.cpp
+++ llvm/lib/Target/Xtensa/XtensaInstrInfo.cpp
@@ -317,7 +317,6 @@ bool XtensaInstrInfo::isBranchOffsetInRange(unsigned BranchOp,
     return true;
   case Xtensa::LOOPEND:
     BrOffset += 4;
-    BrOffset += 3 * 3;   // 2*NOP + LOOP instrucions
     assert((BrOffset <= 0) && "Wrong hardware loop");
     return true;
   case Xtensa::BR_JT:
